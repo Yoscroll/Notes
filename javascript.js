54bd6f3c33bc2d68ec7f3d798a6ad7afa7c222fe
Yoscroll <ianhenry.c808@gmail.com>
2016-10-17 08:44:50 -1000

    added javascript

---
+++ javascript.js
@@ -0,0 +1,32 @@
+//other: objects and arrays
+
+//this is a string
+var myName = "Ian";
+
+//this is an interger
+var myAge = 15;
+
+//boolen type
+var boy = true;
+
+//unassigned cariable
+undefined
+
+//not a number
+NaN
+
+var camelCase = "yay";
+
+/*
+!== not equals
+> greater than 
+< less than 
+>= greater than or equal to 
+<= less than or equal to 
+
+&& - and 
+|| - or
+! - not; put in front of expressions to negate them
+Combine with Boolean operators to create more complex expressions 
+You can also nest conditional statements within each other
+*/
