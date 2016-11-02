69e5b20eb0d58403934267663451b7dd44ea15e4
Yoscroll <ianhenry.c808@gmail.com>
2016-11-02 08:08:07 -1000

    11216

--- javascript.js
+++ javascript.js
@@ -30,3 +30,43 @@ var camelCase = "yay";
 Combine with Boolean operators to create more complex expressions 
 You can also nest conditional statements within each other
 */
+
+//fuction-executed block
+function () {
+}
+
+function nameOfFunction (){
+  console.log("hello i am a new function");
+}
+
+//invoking a function
+nameOfFunction();
+
+//parameters
+function hello(a, b) {
+  console.log(a+b);
+}
+hello(10,11);
+
+function bai(c,d){
+  console.log(c-b);
+  return c+d;
+}
+
+bai(20,10);
+
+var subtract = bai(30,12);
+console.log(subtract);
+
+function createGreeting(name,age){
+  if (age<32){
+    console.log("hello " + name);
+    return "hello " + name;
+  }
+  else{
+    console.log("hello Mr."+name);
+    return "hello Mr." + name;
+  }
+}
+createGreeting("Ian",15);
+
