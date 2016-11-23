c600527bdaf4f9deecdf765fe09bc6a687ce1e84
Yoscroll <ianhenry.c808@gmail.com>
2016-11-23 09:00:36 -1000

    added arrays and objects file

---
+++ array_object.js
@@ -0,0 +1,15 @@
+//creating an array
+var tan = ["John","jon","cool",3,11,7,9,null,undefined,["no shirt",27,null]];
+
+console.log(tan[9][0]);//access "no shirt" (an array in an array)
+
+var yoloSwag={
+  isaaha : "beast",
+  hair: "yellow",
+  sayings: ["wait what?","hold me im cold","yoloha"],
+  position: "kicker"
+};
+
+console.log(yoloSwag.sayings[2]);//logs "yoloha"
+console.log(yoloSwag["sayings"][2]);//logs "yoloha" another way
+
